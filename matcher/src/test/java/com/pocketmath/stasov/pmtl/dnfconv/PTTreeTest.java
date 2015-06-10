64ecbd4481032bc7dcc0d8e8da97684c34b678f3
erictucker <eric@pocketmath.com>
2015-06-09 21:25:12 -0500

    Move PTTreeTest to pmtl.dnfconv

--- matcher/src/test/java/com/pocketmath/stasov/pmtl/dnfconv/PTTreeTest.java
+++ matcher/src/test/java/com/pocketmath/stasov/pmtl/dnfconv/PTTreeTest.java
@@ -1,7 +1,5 @@
-package com.pocketmath.stasov.dnfconv;
+package com.pocketmath.stasov.pmtl.dnfconv;
 
-import com.pocketmath.stasov.dnfconv.DNFConvTree;
-import com.pocketmath.stasov.dnfconv.DNFConvTreeBuilder;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
