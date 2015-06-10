10f8c2deb3679c855b9393c8af5aacae57a9408d
erictucker <eric@pocketmath.com>
2015-06-09 21:23:06 -0500

    add exception

---
+++ matcher/src/main/java/com/pocketmath/stasov/pmtl/PocketTLLanguageException.java
@@ -0,0 +1,26 @@
+package com.pocketmath.stasov.pmtl;
+
+/**
+ * Created by etucker on 6/9/15.
+ */
+public class PocketTLLanguageException extends Exception {
+    public PocketTLLanguageException() {
+        super();
+    }
+
+    public PocketTLLanguageException(Throwable cause) {
+        super(cause);
+    }
+
+    public PocketTLLanguageException(String message) {
+        super(message);
+    }
+
+    public PocketTLLanguageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    protected PocketTLLanguageException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
