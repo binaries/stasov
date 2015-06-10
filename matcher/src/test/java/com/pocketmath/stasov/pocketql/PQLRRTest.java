0d8da062f09c37cab18831d470f21bf7963f0bc6
erictucker <eric@pocketmath.com>
2015-06-09 23:12:19 -0500

    Match tests

--- matcher/src/test/java/com/pocketmath/stasov/pocketql/PQLRRTest.java
+++ matcher/src/test/java/com/pocketmath/stasov/pocketql/PQLRRTest.java
@@ -12,44 +12,58 @@ public class PQLRRTest extends PQLRRTestBase {
 
     @Test
     public void test1() throws Exception {
-        Assert.assertEquals(parse("city=austin"), "city=austin");
+        assertEqualsIgnoreCase(parse("city=austin"), "city=austin");
     }
 
     @Test
     public void test2() throws Exception {
-        Assert.assertEquals(parse("not not city=austin"), "city=austin");
+        assertEqualsIgnoreCase(parse("not not city=austin"), "city=austin");
     }
 
     @Test
     public void test3_1() throws Exception {
-        Assert.assertEquals(
+        assertEqualsIgnoreCase(
                 parse("(city=austin or city=singapore) and devicetype=iphone"),
                 "city=austin AND devicetype=iphone OR city=singapore AND devicetype=iphone");
     }
 
     @Test
     public void test3_2() throws Exception {
-        Assert.assertEquals(
+        assertEqualsIgnoreCase(
                 parse("devicetype=iphone and (city=austin or city=singapore)"),
                 "city=austin AND devicetype=iphone OR city=singapore AND devicetype=iphone");
     }
 
     @Test
     public void test3_3() throws Exception {
-        Assert.assertEquals(
+        assertEqualsIgnoreCase(
                 parse("(a=1 or b=2) and (c=3 or d=4)"),
-                "a=1 and c=3 or a=1 and d=4 or b=2 and c=3 or b=2 and d=4");
+                "a=1 AND c=3 OR a=1 AND d=4 OR b=2 AND c=3 OR b=2 AND d=4");
+    }
+
+    @Test
+         public void test3_4() throws Exception {
+        assertEqualsIgnoreCase(
+                parse("a=1 AND b=2 AND c=3"),
+                "a=1 AND b=2 AND c=3");
+    }
+
+    @Test
+    public void test3_5() throws Exception {
+        assertEqualsIgnoreCase(
+                parse("a=1 AND (b=2 AND c=3)"),
+                "a=1 AND b=2 AND c=3");
     }
 
     @Test
     public void test4() throws Exception {
-        Assert.assertEquals(
+        assertEqualsIgnoreCase(
                 parse("((city=austin))"), "city=austin");
     }
 
     @Test
     public void test5() throws Exception {
-        Assert.assertEquals(
+        assertEqualsIgnoreCase(
                 parse("(city=austin oR city=singapore or city=vancouver) and devicetype=iphone)"),
                 "city=austin AND devicetype=iphone OR city=singapore AND devicetype=iphone OR city=vancouver AND devicetype=iphone");
     }
@@ -62,7 +76,7 @@ public class PQLRRTest extends PQLRRTestBase {
         final String expected =
                 "e=g and w=x";
 
-        Assert.assertEquals(parse(input), expected);
+        assertEqualsIgnoreCase(parse(input), expected);
     }
 
     @Test
@@ -73,7 +87,7 @@ public class PQLRRTest extends PQLRRTestBase {
         final String expected =
                 "e=g or w=x";
 
-        Assert.assertEquals(parse(input), expected);
+        assertEqualsIgnoreCase(parse(input), expected);
     }
 
     @Test
--- matcher/src/test/java/com/pocketmath/stasov/pocketql/PQLRRTestBase.java
+++ matcher/src/test/java/com/pocketmath/stasov/pocketql/PQLRRTestBase.java
@@ -4,15 +4,30 @@ import com.pocketmath.pocketql.grammars.nfchecker.PocketQLNFCheckerLexer;
 import com.pocketmath.pocketql.grammars.nfchecker.PocketQLNFCheckerParser;
 import com.pocketmath.pocketql.grammars.transformer.PocketQLAllLexer;
 import com.pocketmath.pocketql.grammars.transformer.PocketQLAllParser;
+import org.antlr.runtime.debug.BlankDebugEventListener;
+import org.antlr.runtime.debug.DebugEventHub;
+import org.antlr.runtime.debug.DebugEventListener;
+import org.antlr.runtime.debug.TraceDebugEventListener;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Parser;
+import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.tree.ParseTreeListener;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import org.testng.Assert;
 
 /**
  * Created by etucker on 4/8/15.
  */
 public class PQLRRTestBase {
 
+    protected final void assertEqualsIgnoreCase(final String s1, final String s2) {
+        Assert.assertEquals(s1.toLowerCase(), s2.toLowerCase());
+    }
+
+    protected final void assertNotEqualsIgnoreCase(final String s1, final String s2) {
+        Assert.assertNotEquals(s1.toLowerCase(), s2.toLowerCase());
+    }
 
     private boolean isNormalForm(final String input) throws Exception {
         if (input == null) throw new IllegalArgumentException("input was null");
@@ -23,9 +38,9 @@ public class PQLRRTestBase {
         final CommonTokenStream tokens = new CommonTokenStream(lexer);
         final PocketQLNFCheckerParser parser = new PocketQLNFCheckerParser(tokens);
 
-        final PocketQLNFCheckerParser.Normal_formContext nfctx = parser.normal_form();
+        final PocketQLNFCheckerParser.StartContext nfctx = parser.start();
 
-        final boolean match = nfctx != null && nfctx.match && nfctx.getText().equals(input);
+        final boolean match = nfctx != null && nfctx.match && input.equals(nfctx.getText());
 
         if (l()) log("normal form match=" + match + "  " + nfctx.getText());
 
@@ -40,12 +55,29 @@ public class PQLRRTestBase {
         System.out.println("[log] " + msg);
     }
 
+    private static class PocketQLAllDebuggableParser extends PocketQLAllParser {
+        public PocketQLAllDebuggableParser(TokenStream input, boolean trace) {
+            super(input);
+            if (trace) {
+                ParseTreeListener debugListener = new PocketQLAllParser.TraceListener();
+                this.addParseListener(debugListener);
+            }
+        }
+    }
+
     protected String parse(final String input) throws Exception {
+        if (input == null) throw new IllegalArgumentException("input was null");
 
         String _input = input;
+        String _last_input = null;
 
         String output = null;
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 100; i++) {
+            if (_input.equals(_last_input)) {
+                if (l()) log("stuck transformation (infinite loop) detected for: " + _input);
+                return null;
+            }
+
             if (isNormalForm(_input)) {
                 if (l()) log("returning, normal form found: " + _input);
                 return _input;
@@ -57,15 +89,17 @@ public class PQLRRTestBase {
 
             nflexer = new PocketQLAllLexer(new ANTLRInputStream(_input));
             nftokens = new CommonTokenStream(nflexer);
-            nfparser = new PocketQLAllParser(nftokens);
+            nfparser = new PocketQLAllDebuggableParser(nftokens, true);
 
-            final PocketQLAllParser.StartContext start = nfparser.start();
-            output = start.value;
+            //final PocketQLAllParser.StartContext start = nfparser.start();
+            final PocketQLAllParser.ExpContext exp = nfparser.exp();
+            output = exp.v;
 
-            System.out.println("transformation cycle: " + output);
+            if (l()) log("transformation cycle: " + output);
 
             if (output == null) return null;
 
+            _last_input = _input;
             _input = output;
 
         }
