f073bcd9dc5ad365637e3f1636f9c0178f86fecb
erictucker <eric@pocketmath.com>
2015-06-21 14:17:04 -0500

    simplify EngineTest methods by encapsulating details

--- matcher/src/test/java/com/pocketmath/stasov/engine/EngineTest.java
+++ matcher/src/test/java/com/pocketmath/stasov/engine/EngineTest.java
@@ -1,5 +1,7 @@
 package com.pocketmath.stasov.engine;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
 import com.pocketmath.stasov.OpportunityData;
 import it.unimi.dsi.fastutil.longs.LongSortedSet;
 import org.testng.Assert;
@@ -9,6 +11,7 @@ import org.testng.annotations.Test;
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -25,44 +28,85 @@ public class EngineTest {
         engine.index(spec, id);
     }
 
-    @Test
-    public void simpleIndexAndQueryTest1() throws IndexingException {
-        Engine engine = new Engine();
+    /**
+     *
+     * @param specifications The PTML specification.
+     * @param opportunityAttributes The attributes from a simulated impression opportunity.
+     * @param expectedResults The expected IO ids.
+     * @throws IndexingException
+     */
+    private void testIndexAndQuery(
+            final Map<Long,String> specifications,
+            final Map<String,String> opportunityAttributes,
+            final long[] expectedResults) throws IndexingException {
 
-        String spec = "DeviceType in (\"iPhone\")";
-        long id = 1;
-        engine.index(spec, id);
+        System.out.println("specs:  " + specifications.toString());
+        System.out.println("opp:    " + opportunityAttributes.toString());
+        System.out.println("expect: " + Arrays.toString(expectedResults));
 
-        OpportunityData opp = new OpportunityData() {
-            @Override
-            protected Set<String> getCity() {
-                return new HashSet<String>(Arrays.asList(new String[]{"Austin"}));
-            }
+        final Engine engine = new Engine();
 
-            @Override
-            protected Set<String> getDeviceId() {
-                return new HashSet<String>(Arrays.asList(new String[]{"abc123"}));
-            }
+        for (final Map.Entry<Long, String> spec : specifications.entrySet()) {
+            final long id = spec.getKey();
+            final String pmtl = spec.getValue();
+            assert(pmtl != null);
+            System.out.println(pmtl);
+            engine.index(pmtl, id);
+        }
 
-            @Override
-            protected Set<String> getDeviceType() {
-                return new HashSet<String>(Arrays.asList(new String[]{"iPhone"}));
-            }
-        };
+        final MapOpportunityData opp = new MapOpportunityData(engine.attrSvc);
+
+        for (final Map.Entry<String, String> attribute : opportunityAttributes.entrySet()) {
+            final String name = attribute.getKey();
+            final String value = attribute.getValue();
+            assert(name != null);
+            assert(value != null);
+            opp.put(name, value);
+        }
+
+        System.out.println("oppObj: " + opp);
+
+        System.out.println("engine: " + engine.prettyPrint());
 
         final LongSortedSet results = engine.query(opp);
 
-        Assert.assertEquals(results.size(), 1);
-        Assert.assertTrue(results.contains(1L));
+        if (expectedResults.length < 1) {
+            Assert.assertEquals(results, null);
+        } else {
+            Assert.assertNotEquals(results, null);
+        }
+
+        { // assert that each result exists in the expected results
+            long[] sortedExpectedResults = expectedResults.clone();
+            Arrays.sort(sortedExpectedResults);
+            for (final Long result : results) {
+                final int found = Arrays.binarySearch(sortedExpectedResults, result.longValue());
+                Assert.assertTrue(found > -1);
+            }
+        }
+
+        { // assert that each expected result exists in the actual results
+            for (final long expectedResult : expectedResults) {
+                Assert.assertTrue(results.contains(expectedResult));
+            }
+        }
+
+        Assert.assertTrue(results.size() == expectedResults.length);
+
     }
 
     @Test
-    public void ssimpleIndexTest2() throws IndexingException {
-        Engine engine = new Engine();
-
-        String spec = "DeviceType = \"iPhone\"";
-        long id = 1;
-        engine.index(spec, id);
+    public void test3() throws IndexingException {
+        testIndexAndQuery(
+                ImmutableMap.<Long,String>of(
+                        1L, "devicetype in (\"iphone\") and city in (\"austin\")"
+                ),
+                ImmutableMap.<String,String>of(
+                        "devicetype", "iphone",
+                        "city", "austin"
+                ),
+                new long[]{ 1L }
+        );
     }
 
 }
