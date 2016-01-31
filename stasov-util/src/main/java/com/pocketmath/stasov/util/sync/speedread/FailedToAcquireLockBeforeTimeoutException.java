e9d56eca16356d27247f4efed32662017c1d86c6
erictucker <eric@pocketmath.com>
2016-01-31 04:29:20 -0600

    reduce visibility of SpeedRead exception constructors to package level

--- stasov-util/src/main/java/com/pocketmath/stasov/util/sync/speedread/FailedToAcquireLockBeforeTimeoutException.java
+++ stasov-util/src/main/java/com/pocketmath/stasov/util/sync/speedread/FailedToAcquireLockBeforeTimeoutException.java
@@ -6,22 +6,22 @@ import java.util.concurrent.ExecutionException;
  * Created by etucker on 1/31/16.
  */
 public class FailedToAcquireLockBeforeTimeoutException extends SRExecutionException {
-    public FailedToAcquireLockBeforeTimeoutException() {
+    FailedToAcquireLockBeforeTimeoutException() {
     }
 
-    public FailedToAcquireLockBeforeTimeoutException(String message) {
+    FailedToAcquireLockBeforeTimeoutException(String message) {
         super(message);
     }
 
-    public FailedToAcquireLockBeforeTimeoutException(String message, Throwable cause) {
+    FailedToAcquireLockBeforeTimeoutException(String message, Throwable cause) {
         super(message, cause);
     }
 
-    public FailedToAcquireLockBeforeTimeoutException(Throwable cause) {
+    FailedToAcquireLockBeforeTimeoutException(Throwable cause) {
         super(cause);
     }
 
-    public FailedToAcquireLockBeforeTimeoutException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+    FailedToAcquireLockBeforeTimeoutException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
     }
 }
