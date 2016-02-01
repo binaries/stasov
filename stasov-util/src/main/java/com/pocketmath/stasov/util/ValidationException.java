5497376b35ef85d5eae5e23dc92ca38ab670adf5
erictucker <eric@pocketmath.com>
2016-02-01 16:36:02 -0600

    exception class for validation

---
+++ stasov-util/src/main/java/com/pocketmath/stasov/util/ValidationException.java
@@ -0,0 +1,26 @@
+package com.pocketmath.stasov.util;
+
+/**
+ * Created by etucker on 2/1/16.
+ */
+public class ValidationException extends Exception {
+
+    public ValidationException() {
+    }
+
+    public ValidationException(String message) {
+        super(message);
+    }
+
+    public ValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ValidationException(Throwable cause) {
+        super(cause);
+    }
+
+    public ValidationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
